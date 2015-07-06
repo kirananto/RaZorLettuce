dac0e5d646258d7ce82e8d28707054517998d060
kirananto <kirananto@gmail.com>
2015-07-06 20:15:51 +0530

    Some More Fixes...
    Yuphoria Booted Stable... Checkpoint 1

--- drivers/soc/qcom/msm_rq_stats.c
+++ drivers/soc/qcom/msm_rq_stats.c
@@ -197,6 +197,7 @@ static ssize_t hotplug_disable_show(struct kobject *kobj,
 }
 
 static struct kobj_attribute hotplug_disabled_attr = __ATTR_RO(hotplug_disable);
+
 #ifdef CONFIG_BRICKED_HOTPLUG
 unsigned int get_rq_info(void)
 {
@@ -208,30 +209,14 @@ unsigned int get_rq_info(void)
         rq = rq_info.rq_avg;
         rq_info.rq_avg = 0;
 
-<<<<<<< HEAD
-=======
-#ifdef CONFIG_BRICKED_HOTPLUG
-unsigned int get_rq_info(void)
-{
-	unsigned long flags = 0;
-        unsigned int rq = 0;
-
-        spin_lock_irqsave(&rq_lock, flags);
-
-        rq = rq_info.rq_avg;
-        rq_info.rq_avg = 0;
-
->>>>>>> a8e0d3bc8693ebd210ea907b8a363ca93daa077e
         spin_unlock_irqrestore(&rq_lock, flags);
 
         return rq;
 }
 EXPORT_SYMBOL(get_rq_info);
 #endif
-<<<<<<< HEAD
-=======
 
->>>>>>> a8e0d3bc8693ebd210ea907b8a363ca93daa077e
+
 static void def_work_fn(struct work_struct *work)
 {
 	int64_t diff;
